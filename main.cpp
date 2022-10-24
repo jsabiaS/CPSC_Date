d63027073df1d2ab79f86ad7d12308fe4a1f5bf4
jsabiaS <116517285+jsabiaS@users.noreply.github.com>
2022-10-24 15:13:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,48 +7,49 @@
 main(int argc, char const *argv[]) {
 
     ifstream file1;
+    ofstream output;
 
     int numDates;
     int month;
     int day;
     int year;
 
+    string temp;
+
     vector<Date> dates;
 
-    file1.open ("dates.txt");
+    file1.open(argv[1]) ;
     if (file1.is_open()) {
-        file1 >> numDates;
+        file1 >> temp;
+        numDates = stoi(temp);
 
-        while (!file1.eof()) {
-            Date date(month, day, year);
+    for (int i = 0; i < numDates; i++) {
+        file1 >> month;
+        file1 >> day;
+        file1 >> year;
 
+        Date date(month, day, year);
+        dates.push_back(date);
 
-            for (int i = 0; i < numDates; i++) {
-                file1 >> month;
-                file1 >> day;
-                file1 >> year;
+        dates[i].setMonth(month);
+        dates[i].setDay(day);
+        dates[i].setYear(year);
 
-                dates.push_back(date);
+    }     
 
-                dates[i].setMonth(month);
-                dates[i].setDay(day);
-                dates[i].setYear(year);
-            }
+    sort(dates.begin(), dates.end(), Date::compare);
 
-        sort(dates.begin(), dates.end(), Date::compare); 
+    output.open(argv[2]);
 
-        }
-
-        for (int i = 0; i < numDates; ++i) {
-            cout << dates[i].print() << endl;
-        }
-
+    for (int i = 0; i < numDates; ++i) {
+        output << dates[i].print() << endl;
+    }
         file1.close();
-        
+        output.close();   
     }
 
     else {
-        cout << "shit" << endl;
+        cout << "Your file did not open" << endl;
     }
 
     return 0;
